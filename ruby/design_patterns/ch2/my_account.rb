6d41e217560eea5dd7acd901b484cd2b83378103
Dennis Zaloznyy <dennis@beachmint.com>
2011-10-23 12:41:51 -0700

    Added a check to see if I am broke

--- ruby/design_patterns/ch2/my_account.rb
+++ ruby/design_patterns/ch2/my_account.rb
@@ -4,6 +4,9 @@ my_account = BankAccount.new 'Dennis'
 my_account.deposit 5
 
 puts my_account.balance
+my_account.withdraw 3
+puts 'I am almost broke' if my_account.balance < 5
+
 
 my_interest_account = InterestBearingAccount.new 'Lili', 3
 my_interest_account.deposit 1000
